3c5592ac5464af4e1d639bafa8710f5f212e7c1f
ICYEZAGATORE <i.icyezagat1@gmail.com>
2024-03-14 02:40:05 -0700

    Square with size

---
+++ python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Write a class Square that defines a square by"""
+
+
+Class Square:
+    """Private instance attribute: size
+    Instantiation with size (no type/value verification)
+    no importation of modules"""
+    def __init__(self, size):
+        self.__size = size
+
