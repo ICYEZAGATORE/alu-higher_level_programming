147450484929cc0a6a6cd118fdc199340b095efd
ICYEZAGATORE <i.icyezagat1@alustudent.com>
2024-03-24 13:19:01 +0000

    Tiku

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""  fetches https://intranet.hbtn.io/status  """
+""" Pythonn script that fetches https://intranet.hbtn.io/status """
 import urllib.request
 
 
 if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        r = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(r)))
-        print("\t- content: {}".format(r))
-        print("\t- utf8 content: {}".format(r.decode("UTF-8")))
+        html = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode("utf-8")))
