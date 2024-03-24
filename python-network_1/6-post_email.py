59aaf74e209d8325c8f221a5aeffc5b964cafac1
ICYEZAGATORE <i.icyezagat1@alustudent.com>
2024-03-24 13:40:25 +0000

    My GitHub!

--- python-network_1/6-post_email.py
+++ python-network_1/6-post_email.py
@@ -1,15 +1,13 @@
-
 #!/usr/bin/python3
 """
-Python script that takes in a URL and an email address,
-sends a POST request to the passed URL with the email as a parameter
+A Python script that takes in a URL and an email address,
+sends a POST request to the passed URL with the email,
+and displays the body of the response
 """
 import requests
 import sys
 
-if __name__ == '__main__':
 
-    new_value = {'email': sys.argv[2]}
-    req = requests.post(sys.argv[1], new_value)
-    print(req.text)
-
+if __name__ == "__main__":
+    r = requests.post(sys.argv[1], data={'email': sys.argv[2]})
+    print(r.text)
